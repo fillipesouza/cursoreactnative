35de767e8adf209dac43f0101931161c3c4a99d2
fillipesouza <ss.fillipe@gmail.com>
2020-04-24 14:33:03 -0300

    Add files via upload

---
+++ models/Virus.js
@@ -0,0 +1,8 @@
+export default class Virus {
+    constructor(imageUrl, title, description, id){
+        this.imageUrl = imageUrl;
+        this.title = title;
+        this.description = description;
+        this.id = id;
+    }
+}
