12417e0d955b234eb172d33a584d87c23206ad1a
fillipesouza <ss.fillipe@gmail.com>
2020-05-11 19:37:57 -0300

    Updating version

---
+++ components/FormComponents.js
@@ -0,0 +1,23 @@
+import { View,  } from 'react-native';
+import { compose } from "recompose";
+import {
+    handleTextInput,
+    withNextInputAutoFocusInput,
+} from "react-native-formik";
+import { TextField } from "react-native-material-textfield";
+import { Dropdown } from 'react-native-material-dropdown';
+
+import { withNextInputAutoFocusForm } from "react-native-formik";
+
+export const MyForm = withNextInputAutoFocusForm(View);
+
+export const MyInput = compose(
+    handleTextInput,
+    withNextInputAutoFocusInput
+)(TextField);
+
+export const MyDropDown = compose(
+    handleTextInput,
+    withNextInputAutoFocusInput
+)(Dropdown);
+
